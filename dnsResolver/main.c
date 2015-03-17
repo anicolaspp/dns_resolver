d35f41a4d62075e14909c9185a3e0d673c8396bb
Nicolas A Perez <anicolaspp@gmail.com>
2015-03-17 00:09:28 -0400

    Initial Commit

---
+++ dnsResolver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dnsResolver
+//
+//  Created by Nicolas A Perez on 3/17/15.
+//  Copyright (c) 2015 Nicolas A Perez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
